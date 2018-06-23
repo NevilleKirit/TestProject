d69caa05bc8ab28ba90dfa94716c06e0512aa4c9
NEVILLE <NEVILLE@NEVILLE-PC>
2018-06-23 19:12:23 +0530

    2nd commit

---
+++ LearnJava/src/myfirstjavaprogram/CreateObjects.java
@@ -0,0 +1,13 @@
+package myfirstjavaprogram;
+
+public class CreateObjects {
+	
+	int age;
+	
+	public void test(){
+		if(age>25){
+			System.out.println("Pass");
+		}
+	}
+
+}
---
+++ LearnJava/src/myfirstjavaprogram/TestObjects.java
@@ -0,0 +1,13 @@
+package myfirstjavaprogram;
+
+public class TestObjects {
+
+	public static void main(String[] args) {
+		CreateObjects obj1=new CreateObjects();
+		
+		obj1.age=26;
+		obj1.test();
+
+	}
+
+}
